6c20db442aad9c12af14a0e88ddf06b953c9365c
Darien Tsai <smartdragondt@gmail.com>
2021-01-07 22:36:01 -0800

    lab done

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,50 @@
  */
 
 exports.view = function(req, res){
-  res.render('index');
+  res.render('index', {
+    "projects":[
+      {
+        "name": "Waiting in line",
+        "image": "lorempixel.people.1.jpeg",
+        "id": "1"
+      },
+      {
+        'name': 'Needfinding',
+        'image': 'lorempixel.city.1.jpeg',
+        'id': '2'
+      },
+      {
+        'name': 'Prototyping',
+        'image': 'lorempixel.technics.1.jpeg',
+        'id': '3'
+      },
+      {
+        'name': 'Heuristic Evaluation',
+        'image': 'lorempixel.abstract.1.jpeg',
+        'id': '4'
+      },
+      {
+        'name': 'Visualization',
+        'image': 'lorempixel.abstract.8.jpeg',
+        'id': '5'
+      },
+      {
+        'name': 'Social design',
+        'image': 'lorempixel.people.2.jpeg',
+        'id': '6'
+      },
+      {
+        'name': 'Gestural interaction',
+        'image': 'lorempixel.technics.2.jpeg',
+        'id': '7'
+      },
+      {
+        'name': 'Design tools',
+        'image': 'lorempixel.city.2.jpeg',
+        'id': '8'
+      },
+
+        
+    ]
+  });
 };
